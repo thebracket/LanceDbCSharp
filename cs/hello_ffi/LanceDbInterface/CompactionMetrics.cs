a2b090fad1019a403dfbdc87446221e84162e95c
Edwin Ma <ema@jsitelecom.com>
2024-11-05 16:22:37 -0500

    chore: depricate compact_files and add optimize

---
+++ cs/hello_ffi/LanceDbInterface/CompactionMetrics.cs
@@ -0,0 +1,9 @@
+﻿namespace LanceDbInterface;
+
+public class CompactionMetrics
+{
+    public int FragmentsRemoved { get; set; } = 0;
+    public int FragmentsAdded { get; set; } = 0;
+    public int FilesRemoved { get; set; } = 0;
+    public int FilesAdded { get; set; } = 0;
+}
