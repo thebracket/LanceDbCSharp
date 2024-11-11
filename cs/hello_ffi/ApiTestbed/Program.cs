7b9a84573079abda15a48e029a0e2835a085976e
Edwin Ma <ema@jsitelecom.com>
2024-11-11 13:01:28 -0500

    chore: test query builder.

--- cs/hello_ffi/ApiTestbed/Program.cs
+++ cs/hello_ffi/ApiTestbed/Program.cs
@@ -2,7 +2,12 @@
 
 using Apache.Arrow;
 using Apache.Arrow.Types;
+using System.Collections.Generic;
 using LanceDbClient;
+using LanceDbInterface;
+using Array = Apache.Arrow.Array;
+
+const int Dimension = 128;
 
 using (var cnn = new Connection(new Uri("file:///tmp/test_lance")))
 {
@@ -16,7 +21,42 @@ using (var cnn = new Connection(new Uri("file:///tmp/test_lance")))
     System.Console.WriteLine("Tables Created: " + table1 + ", " + table2);
     // So its now expected to see 2 tables
     ListTables(cnn);
+
+    int numEntries = 3;
+    System.Console.WriteLine($"{numEntries} records added.");
+    table1.Add(GetBatches(numEntries));
+    System.Console.WriteLine($"Table 1 row count (expect {numEntries}): {table1.CountRows()}" );
     
+    string[] columns = new[] { "id" };    
+    ILanceMergeInsertBuilder builder = table1.MergeInsert(columns);
+    numEntries = 4;
+    builder.WhenMatchedUpdateAll().WhenNotMatchedInsertAll().Execute(GetBatches(numEntries));
+    System.Console.WriteLine($"Table 1 row count (expect {numEntries}): {table1.CountRows()}" );
+    
+    //table1.CreateIndex("vector", Metric.Cosine, 256, 16);
+    //table1.CreateScalarIndex("id");
+
+    var queryBuilder = table1.Search();
+    Console.WriteLine(queryBuilder.Limit(3).ExplainPlan());
+    //var result = queryBuilder.Limit(3).WhereClause("id > 0").WithRowId(true).ToList();
+
+    List<float> vector = new List<float>();
+    for (int i = 0; i < Dimension; i++)
+    {
+        vector.Add(0.3f);
+    }   
+    var result = queryBuilder.Vector(vector).Limit(3).WithRowId(true).ToList();
+    
+    foreach (var row in result)
+    {
+        foreach (string key in row.Keys)
+        {
+            Console.WriteLine($"{key}: {row[key]}");
+        }
+        Console.WriteLine();
+    }
+    
+
     // Now we'll drop table1
     cnn.DropTable("table1");
     System.Console.WriteLine("Table 1 Dropped");
@@ -68,7 +108,7 @@ Schema GetSchema()
     var itemField = new Field("item", FloatType.Default, nullable: true);
 
     // Define the FixedSizeListType with the "item" field and a fixed size of 128
-    var vectorType = new FixedSizeListType(itemField, listSize: 128);
+    var vectorType = new FixedSizeListType(itemField, listSize: Dimension);
 
     // Define the "vector" field (FixedSizeList, nullable)
     var vectorField = new Field("vector", vectorType, nullable: true);
@@ -82,4 +122,57 @@ Schema GetSchema()
     var schema = new Schema(fields, metadata);
 
     return schema;
+}
+
+IEnumerable<RecordBatch> GetBatches(int numEntries)
+{
+    
+    var idArray = new Int32Array.Builder().AppendRange(Enumerable.Range(1, numEntries)).Build();
+
+    Random random = new Random();
+    float[] randomFloatArray = new float[Dimension * numEntries];
+    float step = 0f;
+    int dimensionIndex = 0;
+    for (int i = 0; i < randomFloatArray.Length; i++)
+    {
+        if (dimensionIndex == Dimension)
+        {
+            step += 1/(float)numEntries;
+            dimensionIndex = 0;  
+        }
+        else
+            dimensionIndex++; 
+        randomFloatArray[i] = step; 
+    }
+    
+    var floatBuffer = new ArrowBuffer.Builder<float>()
+        .AppendRange(randomFloatArray)
+        .Build();
+    ArrayData arrayData = new ArrayData(FloatType.Default, Dimension * numEntries, 0, 0, new [] {ArrowBuffer.Empty, floatBuffer});
+    var floatArray = new FloatArray(arrayData);
+    
+    var listArrayData = new ArrayData(
+        new FixedSizeListType(new Field("item", FloatType.Default, nullable: true), Dimension),
+        length: numEntries,
+        nullCount: 0,
+        offset: 0,
+        buffers: new[] { ArrowBuffer.Empty },
+        children: new[] { floatArray.Data }
+    );
+    var fixedSizeListArray = new FixedSizeListArray(listArrayData);
+
+    RecordBatch recordBatch = new RecordBatch(GetSchema(), new IArrowArray[] {idArray, fixedSizeListArray},  idArray.Length);
+    RecordBatch[] batches = new[] { recordBatch };
+
+    return batches;
+}
+
+FloatArray GetFloatArray(float value)
+{
+    var builder = new FloatArray.Builder();
+    for (int i = 0; i < Dimension; i++)
+    {
+        builder.Append(value);
+    }
+    return builder.Build();
 }
