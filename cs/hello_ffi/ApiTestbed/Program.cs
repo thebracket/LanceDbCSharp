19811eecb1b3b00b71f3627ba8015ef443760d18
Edwin Ma <ema@jsitelecom.com>
2024-11-26 12:43:15 -0500

    test reranker()

--- cs/hello_ffi/ApiTestbed/Program.cs
+++ cs/hello_ffi/ApiTestbed/Program.cs
@@ -131,7 +131,13 @@ using (var cnn = new Connection(new Uri("file:///tmp/test_lance")))
         .Rerank(new RrfReranker())
         .ToListAsync();
     PrintDictList(testRrf);
-    
+
+    Console.WriteLine("Reranking '12' with RRF");
+    var testRrfSync = table2.Search(vector, "vector", queryType: QueryType.Hybrid)
+        .Rerank(new RrfReranker())
+        .ToBatches(2);
+    PrintBatches(testRrfSync);
+
     // Now we'll drop table2
     await cnn.DropTableAsync("table2");
     System.Console.WriteLine("Table 2 Dropped (Asynchronously)");
