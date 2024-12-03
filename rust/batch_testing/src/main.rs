e014e424c206776f254041dbc6107578dfee3595
Herbert Wolverson <herberticus@gmail.com>
2024-12-03 10:36:06 -0600

    Adds a Rust-based test program that checks that batch-size does something.

---
+++ rust/batch_testing/src/main.rs
@@ -0,0 +1,83 @@
+//! This is following the guide at:
+//! https://lancedb.github.io/lancedb/basic/
+
+use anyhow::Result;
+use arrow_array::types::Float32Type;
+use arrow_array::{FixedSizeListArray, Int32Array, RecordBatch, RecordBatchIterator};
+use arrow_schema::{DataType, Field, Schema};
+use lancedb::arrow::IntoArrow;
+use lancedb::connect;
+use std::sync::Arc;
+use futures::TryStreamExt;
+use lancedb::query::{ExecutableQuery, QueryBase, QueryExecutionOptions};
+
+#[tokio::main]
+async fn main() -> Result<()> {
+    // Connect to the database
+    let uri = "/tmp/test-batching";
+    let db = connect(uri).execute().await?;
+
+    // Create some initial data
+    let initial_data = create_some_records()?;
+    let tbl = db
+        .create_table("my_table", initial_data)
+        .execute()
+        .await?;
+
+    // Query the data
+    let mut options = QueryExecutionOptions::default();
+    options.max_batch_length = 1024;
+    let mut stream = tbl
+        .query()
+        .execute_with_options(options)
+        .await
+        .expect("Query creation failed");
+    let mut count = 0;
+    while let Some(_batch) = stream.try_next().await? {
+        println!("Received Batch {count}");
+        count += 1;
+    }
+
+    // Clean up
+    db.drop_db().await?;
+
+    // Return success if nothing goes wrong
+    Ok(())
+}
+
+// Taken from: https://github.com/lancedb/lancedb/issues/1153
+fn create_some_records() -> Result<impl IntoArrow> {
+    const TOTAL: usize = 8192;
+    const DIM: usize = 128;
+
+    let schema = Arc::new(Schema::new(vec![
+        Field::new("id", DataType::Int32, false),
+        Field::new(
+            "vector",
+            DataType::FixedSizeList(
+                Arc::new(Field::new("item", DataType::Float32, true)),
+                DIM as i32,
+            ),
+            true,
+        ),
+    ]));
+    let batches = RecordBatchIterator::new(
+        vec![RecordBatch::try_new(
+            schema.clone(),
+            vec![
+                Arc::new(Int32Array::from_iter_values(0..TOTAL as i32)),
+                Arc::new(
+                    FixedSizeListArray::from_iter_primitive::<Float32Type, _, _>(
+                        (0..TOTAL).map(|_| Some(vec![Some(1.0); DIM])),
+                        DIM as i32,
+                    ),
+                ),
+            ],
+        )
+            ?]
+            .into_iter()
+            .map(Ok),
+        schema.clone(),
+    );
+    Ok(Box::new(batches))
+}
